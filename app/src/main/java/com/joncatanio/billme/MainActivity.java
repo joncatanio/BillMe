ba3a0457a12c4e950093abb0030b85b515b6f979
Mark McKinney <markp.mckinney@gmail.com>
2016-11-04 14:03:38 -0700

    Got tablet layout working

--- app/src/main/java/com/joncatanio/billme/MainActivity.java
+++ app/src/main/java/com/joncatanio/billme/MainActivity.java
@@ -1,11 +1,11 @@
 package com.joncatanio.billme;
 
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
@@ -14,21 +14,22 @@ import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class MainActivity extends AppCompatActivity {
-    private static String TAG = "BillMe";
+    private static final String TAG = "BillMe";
     private Drawer drawer;
     private AccountHeader accountHeader;
+
+    private static final int DASHBOARD = 0;
+    private static final int GROUPS = 1;
+    private static final int ACCOUNT = 2;
+    private static final int SETTINGS = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         accountHeader = new AccountHeaderBuilder()
                 .withActivity(this)
@@ -41,34 +42,32 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         int accent = getResources().getColor(R.color.colorAccent);
-        drawer = new DrawerBuilder()
+
+        DrawerBuilder drawerBuilder = new DrawerBuilder()
                 .withActivity(this)
-                .withToolbar(toolbar)
-                .withActionBarDrawerToggle(true)
-                .withActionBarDrawerToggleAnimated(true)
                 .withAccountHeader(accountHeader)
                 .addDrawerItems(
-                        new PrimaryDrawerItem().withIdentifier(1).withName("Dashboard").withIcon(android.R.drawable.ic_menu_manage).withSelectedIconColor(accent).withSelectedIconColorRes(R.color.accent),
-                        new PrimaryDrawerItem().withIdentifier(2).withName("Groups").withIcon(android.R.drawable.ic_menu_add).withSelectedIconColor(accent),
-                        new PrimaryDrawerItem().withIdentifier(3).withName("Account").withIcon(android.R.drawable.ic_menu_compass).withSelectedIconColor(accent),
+                        new PrimaryDrawerItem().withIdentifier(DASHBOARD).withName("Dashboard").withIcon(android.R.drawable.ic_menu_manage).withSelectedIconColor(accent).withSelectedIconColorRes(R.color.accent),
+                        new PrimaryDrawerItem().withIdentifier(GROUPS).withName("Groups").withIcon(android.R.drawable.ic_menu_add).withSelectedIconColor(accent),
+                        new PrimaryDrawerItem().withIdentifier(ACCOUNT).withName("Account").withIcon(android.R.drawable.ic_menu_compass).withSelectedIconColor(accent),
                         new DividerDrawerItem(),
-                        new PrimaryDrawerItem().withIdentifier(4).withName("Settings").withIcon(android.R.drawable.ic_menu_preferences).withSelectedIconColor(accent)
+                        new PrimaryDrawerItem().withIdentifier(SETTINGS).withName("Settings").withIcon(android.R.drawable.ic_menu_preferences).withSelectedIconColor(accent)
                 )
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
                         switch ((int)drawerItem.getIdentifier()) {
-                            case 1:
-                                Log.d(TAG, "1");
+                            case DASHBOARD:
+                                Log.d(TAG, "dashboard");
                                 break;
-                            case 2:
-                                Log.d(TAG, "2");
+                            case GROUPS:
+                                Log.d(TAG, "groups");
                                 break;
-                            case 3:
-                                Log.d(TAG, "3");
+                            case ACCOUNT:
+                                Log.d(TAG, "account");
                                 break;
-                            case 4:
-                                Log.d(TAG, "4");
+                            case SETTINGS:
+                                Log.d(TAG, "settings");
                                 break;
                             default:
                                 Log.d(TAG, "default");
@@ -76,7 +75,29 @@ public class MainActivity extends AppCompatActivity {
                         }
                         return false;
                     }
-                })
-                .build();
+                });
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            drawer = drawerBuilder
+                    .withToolbar(toolbar)
+                    .withActionBarDrawerToggle(true)
+                    .withActionBarDrawerToggleAnimated(true)
+                    .build();
+        } else {
+            drawer = drawerBuilder.buildView();
+            FrameLayout drawerLayout = (FrameLayout) findViewById(R.id.drawerLayout);
+            drawerLayout.addView(drawer.getSlider());
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer != null && drawer.isDrawerOpen()) {
+            drawer.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
