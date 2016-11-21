4e19b8539f2caf839d9660262c5f70f9c1895343
Jon Catanio <joncatanio@gmail.com>
2016-11-21 15:33:33 -0800

    Visual updates to group list view

--- app/src/main/java/com/joncatanio/billme/GroupViewHolder.java
+++ app/src/main/java/com/joncatanio/billme/GroupViewHolder.java
@@ -3,6 +3,7 @@ package com.joncatanio.billme;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.joncatanio.billme.model.GroupShort;
@@ -11,18 +12,27 @@ public class GroupViewHolder extends RecyclerView.ViewHolder implements View.OnC
     public static final String GROUP_ID = "com.joncatanio.billme.GROUP_ID";
     private GroupShort listItem;
     private TextView groupName;
+    private TextView groupBalance;
+    private ImageView groupImg;
 
     public GroupViewHolder(View itemView) {
         super(itemView);
         itemView.setOnClickListener(this);
 
         groupName = (TextView) itemView.findViewById(R.id.list_group_name);
+        groupBalance = (TextView) itemView.findViewById(R.id.list_group_balance);
+        groupImg = (ImageView) itemView.findViewById(R.id.list_group_img);
     }
 
     public void bind(final GroupShort listItem) {
         this.listItem = listItem;
 
         groupName.setText(listItem.getGroupName());
+        if (listItem.getAmtOwedAsGroup().equals("None")) {
+            groupBalance.setText("Group Balance: $0.00");
+        } else {
+            groupBalance.setText("Group Balance: $" + listItem.getAmtOwedAsGroup());
+        }
     }
 
     @Override
